059c2e83d6381d593508246e658df4022ed8fe68
Mikael <unnamemik@gmail.com>
2022-09-13 16:52:09 +0300

    example005

--- example002/Program.cs
+++ example002/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
